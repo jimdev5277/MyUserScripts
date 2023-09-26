7544a10467e31c014eacf0c188f4329cd6e2996e
jimzhuang-mac <jimzhuang_mac@jimzhuang-macdeiMac.local>
2023-09-26 17:51:31 +0800

    feat: 10执行任务后停顿3s的demo

--- asyncTask.js
+++ asyncTask.js
@@ -1,31 +1,35 @@
 // 创建一个包含100个任务的数组
 const tasks = Array.from({ length: 100 }, (_, index) => () => asyncTask(index));
+const taskLength = tasks.length;
+
+let currentIndex = 0;
+const results = [];
 
 // 定义一个异步函数来执行任务
 async function executeTasks() {
   const concurrency = 10; // 同时执行的任务数
-  const results = [];
-  let currentIndex = 0;
-
+  
   async function runNextTask() {
     const taskIndex = currentIndex++;
     if (taskIndex >= tasks.length) {
       return;
     }
-
     const task = tasks[taskIndex];
     const result = await task();
     results.push(result);
-
-    // 继续执行下一个任务
-    await runNextTask();
   }
 
   // 同时启动最多10个任务
-  await Promise.all(Array(concurrency).fill(null).map(runNextTask));
-
-  console.log("所有任务已完成");
-  console.log(results);
+  Promise.all(Array(concurrency).fill(null).map(runNextTask)).then(() => {
+    if (results.length >= taskLength) {
+      console.log("所有任务已完成");
+      console.log(results);
+    } else {
+      console.log('等待3秒');
+      setTimeout(executeTasks, 3000); // 如果还有任务，则等待 3 秒后继续执行
+    }
+  });
+  
 }
 
 // 模拟一个异步任务，这里使用setTimeout来模拟异步操作
